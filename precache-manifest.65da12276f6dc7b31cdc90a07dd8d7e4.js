23974b8125cdb654c9f26d8ff2ed2116e07ce5b6
kiranpuli <pulikiran070899@gmail.com>
2020-03-05 20:25:23 +0530

    Updates

---
+++ precache-manifest.65da12276f6dc7b31cdc90a07dd8d7e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09c59476e76d4da75f24e8fa8aecd2c",
+    "url": "/RecipeSearch/index.html"
+  },
+  {
+    "revision": "7bc9235478b5e3fd54fe",
+    "url": "/RecipeSearch/static/css/main.fa0cfb26.chunk.css"
+  },
+  {
+    "revision": "316fe34f780067e66c1e",
+    "url": "/RecipeSearch/static/js/2.662d7688.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RecipeSearch/static/js/2.662d7688.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bc9235478b5e3fd54fe",
+    "url": "/RecipeSearch/static/js/main.a6001387.chunk.js"
+  },
+  {
+    "revision": "9b0dc8da7d513b6c56a1",
+    "url": "/RecipeSearch/static/js/runtime-main.802dc62a.js"
+  },
+  {
+    "revision": "c2d14d7fc11e2f435135e9ff12c3207b",
+    "url": "/RecipeSearch/static/media/bg.c2d14d7f.svg"
+  }
+]);
